98cfc56e2661e1f503b2af9e82f4bdd50dcedade
reecemjones15 <reecemjones15@gmail.com>
2021-11-09 21:03:48 -0500

    Create query, render it, and change functional component GeneralView.js to class-based component

--- src/Components/Data/General/GeneralView.js
+++ src/Components/Data/General/GeneralView.js
@@ -2,24 +2,51 @@
 import TotalCount from './TotalCount';
 
 // MAIN DEPENDENCIES
-import React from 'react'
+import React, { Component } from 'react'
+import neo4j from "neo4j-driver/lib/browser/neo4j-web";
+// HELPER FILES
+import credentials from "../../../credentials.json";
+import * as helper from "../../Utils/Helpers.js";
+import * as query from "../../Utils/Queries.js";
 
 
 
-// FUNCTIONAL COMPONENT ////////////////////////////////////////////////////////
+class GeneralView extends Component {
 
-function GeneralView(props) {
+//STATE, PROPS, DRIVER INFO, & BINDS
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalPeople: '',
+    }
 
-  // RETURNS PLACEHOLDER
-  return ( 
-    <>
-      <div className="d-flex">
-        <TotalCount type="Nodes" queryResult={8832} /* PUT YOUR CUSTOM QUERY HERE */ />
-        <TotalCount type="Relationships" queryResult={98} /* PUT YOUR CUSTOM QUERY HERE */ />
-        <TotalCount type="People" queryResult={12489} />
-      </div>
-    </>
-  )
+    // INITIATE NEO4J INSTANCE
+    this.driver = neo4j.driver(
+      credentials.port,
+      neo4j.auth.basic(credentials.username, credentials.password),
+      { disableLosslessIntegers: true }
+    );
+    // BIND UTILITY FUNCTIONS TO THIS CONTEXT
+    this.fetchTotalPeople = query.fetchTotalPeople.bind(this);
+  }
+
+//RUN ON COMPONENT MOUNT //////////////////////////////////////////////////////
+  componentDidMount() {
+    this.fetchTotalPeople();
+  }
+
+//RENDER //////////////////////////////////////////////////////////////////////
+  render() {
+    return ( 
+      <>
+        <div className="d-flex">
+          <TotalCount type="Nodes" queryResult={8832} /* PUT YOUR CUSTOM QUERY HERE */ />
+          <TotalCount type="Relationships" queryResult={98} /* PUT YOUR CUSTOM QUERY HERE */ />
+          <TotalCount type="People" queryResult={this.state.totalPeople} />
+        </div>
+      </>
+    )
+  }
 
 }
 
